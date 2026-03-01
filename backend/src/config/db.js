8f450e64aea9e5d6598e1065947f3e4412d5605c
jasvant maddheshiya <jaswantk8423@gmail.com>
2026-03-01 19:23:50 +0530

    chore: setup mongodb connection and env config

---
+++ backend/src/config/db.js
@@ -0,0 +1,12 @@
+import mongoose, { mongo } from "mongoose";
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Mongodb connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./config/db.js";
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`TaskFlow server running on port ${PORT}`);
