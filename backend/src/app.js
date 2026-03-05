867ae6aa02436fd51a2350dc09c2d482943d4051
jasvant maddheshiya <jaswantk8423@gmail.com>
2026-03-05 10:05:21 +0530

    feat: add register user controller

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
+import router from "./routes/auth.routes.js";
 import globalErrorHandler from "./middlewares/error.middleware.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", router);
 app.use(globalErrorHandler);
 export default app;
---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,40 @@
+import User from "../models/User.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js";
+import apiResponse from "../utils/apiResponse.js";
+import generateToken from "../utils/generateToken.js";
+// Register User Controller
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new ApiError(400, "Email and password are required");
+  }
+  if (password.length < 6) {
+    throw new ApiError(400, "Password must be 6 character");
+  }
+  // Check existing user
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    throw new ApiError(409, "User already existed");
+  }
+
+  //create user
+  const user = await User.create({
+    email,
+    password
+  });
+  const token = await generateToken(user._id);
+  res
+    .status(201)
+    .json(
+      new apiResponse(
+        201,
+        { id: user._id, email: user.email, role: user.role, token: token },
+        "User registered successfully"
+      )
+    );
+});
+
+
+
+export { registerUser };
