dab7672f2dcfa65aef12486be3fa59ba85fe1d8d
jasvant maddheshiya <jaswantk8423@gmail.com>
2026-03-12 11:03:24 +0530

    feat: add status priority filtering to get tasks API

--- backend/src/controllers/task.controller.js
+++ backend/src/controllers/task.controller.js
@@ -25,9 +25,15 @@ const getTasks = asyncHandler(async (req, res) => {
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
-  const tasks = await Task.find({ createdBy: req.user._id })
-    .skip(skip)
-    .limit(limit);
+  //filter
+  const filter = { createdBy: req.user._id };
+  if (req.query.status) {
+    filter.status = req.query.status;
+  }
+  if (req.query.priority) {
+    filter.priority = req.query.priority;
+  }
+  const tasks = await Task.find(filter).skip(skip).limit(limit);
   if (!tasks) {
     throw new ApiError(404, "No Task");
   }
