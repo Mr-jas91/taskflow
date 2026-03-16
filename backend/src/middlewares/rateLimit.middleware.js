8263b1d4f30ae2eba503f21810c2794831ed01ca
jasvant maddheshiya <jaswantk8423@gmail.com>
2026-03-16 21:53:31 +0530

    add login rate limit middleware

---
+++ backend/src/middlewares/rateLimit.middleware.js
@@ -0,0 +1,23 @@
+import rateLimit from "express-rate-limit";
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many requests, please try again later"
+  }
+});
+const loginLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 5,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many Login attempt, Please Try again later"
+  }
+});
+export { apiLimiter,loginLimiter };
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/auth.controller.js";
+import { loginLimiter } from "../middlewares/rateLimit.middleware.js";
 const router = express.Router();
 router.post("/register", registerUser);
-router.post("/login", loginUser)
+router.post("/login", loginLimiter, loginUser);
 export default router;
