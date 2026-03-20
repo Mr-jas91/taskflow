ff26d9e0ff156dbd0200cc545dbba5c57586611d
jasvant maddheshiya <jaswantk8423@gmail.com>
2026-03-20 22:24:26 +0530

    fix: validate middleware

--- backend/src/middlewares/validate.middleware.js
+++ backend/src/middlewares/validate.middleware.js
@@ -1,11 +1,12 @@
 import { validationResult } from "express-validator";
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
-const validate = asyncHandler(async (req, res) => {
+const validate = asyncHandler(async (req, res, next) => {
   const errors = validationResult(req);
-  console.log(errors.errors[0]);
+  console.log("error", errors.errors[0]);
   if (!errors.isEmpty()) {
     throw new ApiError(400, errors.errors[0].msg);
   }
+  next();
 });
 export default validate;
