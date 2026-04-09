e694fbea20c6cf15502385a2674ec9247616e65f
jasvant maddheshiya <jaswantk8423@gmail.com>
2026-04-09 23:02:38 +0530

    fix: CORS and ENV data fixed in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import cors from "cors";
 import router from "./routes/auth.routes.js";
@@ -5,7 +7,14 @@ import taskRoutes from "../src/routes/task.routes.js";
 import globalErrorHandler from "./middlewares/error.middleware.js";
 import { apiLimiter } from "./middlewares/rateLimit.middleware.js";
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_END,
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 200
+  })
+);
 app.use(express.json());
 app.use(apiLimiter);
 app.use("/api/auth", router);
